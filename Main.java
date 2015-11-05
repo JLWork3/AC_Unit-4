72d267d39d99d5ce30b0ea1546140f246440e764
TL <janneisyvidals@gmail.com>
2015-11-04 23:30:55 -0500

    HW-10-11_main git commit -m HW-10-11_main [
    wq
    wq
    git commit -m HW-10-11_main

--- Main.java
+++ Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        //given an integer, write code to print out its bitstring
+        //What is 52 in octal (base 8)?
+       System.out.println(Integer.toOctalString(52));
+        //OR
+        System.out.println(Integer.toString(52,8));
+//Given an integer, write code to print out the number of 1s in its bitstring
+        
     }
-}
+
+    }
+
